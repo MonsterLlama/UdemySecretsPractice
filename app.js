88dea7ffdf8f05917627421f801d40bdb40cded6
MonsterLlama <MonsterLlama@gmail.com>
2023-02-09 22:58:33 -0800

    Cleaning up comments...

--- app.js
+++ app.js
@@ -5,28 +5,14 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const ejs        = require('ejs');
 
-
 // Database Backend access
 const mongoose   = require('mongoose');
-//const encrypt    = require('mongoose-encryption');
-
-// MD5 Hashing
-// const md5          = require('md5');
-
-// const bcrypt       = require('bcrypt');
-// const saltRounds   = parseInt(process.env.SALT_ROUNDS);
 
 const session               = require('express-session');
 const passport              = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
-
-// console.log(typeof(express)); // function
-// console.log(typeof(app));     // function
-// console.log(app);
-// console.log(express.static);
-// console.log(express.static('public'));
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -40,11 +26,9 @@ app.use(session({
     cookie: {secure: false}
   }));
 
-// Tell express.js to use passport and use passport to deal with the sessions
+// Tell express.js to use passport and to use passport to deal with the sessions
 app.use(passport.initialize());
 app.use(passport.session());
-
-//console.log(typeof(mongoose));
 
 mongoose.set('strictQuery', true);
 mongoose.connect(`mongodb+srv://${process.env.AUTH_LOGIN}:${process.env.AUTH_PW}@${process.env.MONGO_DB_CLUSTER}.mongodb.net/userDB`,
@@ -54,9 +38,6 @@ const userSchema = mongoose.Schema({
     email:    String,
     password: String
 });
-
-//  Set up the Mongoose encryption..
-//userSchema.plugin(encrypt, {secret: process.env.DB_Secret, encryptedFields:['password']});
 
 // Setup the Passport-Local-Mongoose
 // passportLocalMongoose is what we're going to use to hash and salt our passwords
@@ -69,8 +50,6 @@ passport.use(userModel.createStrategy());
 
 passport.serializeUser(userModel.serializeUser());
 passport.deserializeUser(userModel.deserializeUser());
-
-
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -150,7 +129,7 @@ app.post('/login',
         else
         {
           passport.authenticate('local')(req, res, () => {
-          res.redirect('/secrets');
+            res.redirect('/secrets');
         });
       }
     });
@@ -162,81 +141,3 @@ const PORT = process.env.PORT | 3000;
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}.`);
 });
-
-//console.log(app);
-
-// app.post('/register', (req, res) => {
-    //    - - - Example of using node.js.bcrypt - - -
-    // bcrypt
-    //   .hash(req.body.password, saltRounds)
-    //   .then(hash => {
-    //       // Store hash in your password DB.
-    //       let newUser = new userModel({
-    //         email:    req.body.username,
-    //         password: hash
-    //       });
-    //
-    //       newUser
-    //         .save()
-    //         .then(doc => {
-    //           res.render('secrets');
-    //         })
-    //         .catch(error => {
-    //           res.send(error);
-    //         });
-    //   })
-    //   .catch(error => {
-    //     res.send(error);
-    //   });
-    // });
-
-    // app.post('/login', (req, res) => {
-    //
-    //     userModel
-    //       .findOne({email: req.body.username})
-    //       .then(foundDocument => {
-    //         if (foundDocument)
-    //         {
-        //    - - - Example of using node.js.bcrypt - - -
-    // bcrypt
-    //   .compare(req.body.password, foundDocument.password)
-    //   .then(result => {
-    //     if (result === true)
-    //     {
-    //       res.render('secrets');
-    //     }
-    //     else {
-    //       // Do something else when the password is incorrect..
-    //       res.send('<h1>No User with that Email/Password found!</h1>');
-    //     }
-    //   })
-    //   .catch(error => {
-    //       res.send('<h1>No User with that Email/Password found!</h1>');
-    //   });
-//     }
-//     else
-//     {
-//         res.send('<h1>No User with that Email/Password found!</h1>');
-//     }
-//   })
-//   .catch(error => {
-//       res.send(error);
-//   });
-//
-// });
-
-
-    // userModel
-      // .findOne({email: req.body.username})
-      // .then(foundDocument => {
-      //   if (foundDocument)
-      //   {
-      //   }
-      //   else
-      //   {
-      //       res.send('<h1>No User with that Email/Password found!</h1>');
-      //   }
-      // })
-      // .catch(error => {
-      //     res.send(error);
-      // });
